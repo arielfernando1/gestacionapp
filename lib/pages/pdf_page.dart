f6e6199a47513276a789c770d1d3e39c3324007e
ariel593 <arielferaguirre.2001@gmail.com>
2023-01-19 19:08:48 -0500

    Contenedor de PDF

---
+++ lib/pages/pdf_page.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:printing/printing.dart';
+
+import '../classes/post.dart';
+import '../firebase_controllers/firestore_controller.dart';
+import 'bitacora.dart';
+
+class PdfPage extends StatefulWidget {
+  final firestore = Firestore();
+  PdfPage({super.key});
+
+  @override
+  State<PdfPage> createState() => _PdfPageState();
+}
+
+class _PdfPageState extends State<PdfPage> {
+  FirebaseAuth? instance;
+  User? u;
+  @override
+  Widget build(BuildContext context) {
+    instance = FirebaseAuth.instance;
+    u = instance!.currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exportar'),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: widget.firestore.listAll(u!.uid),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return PdfPreview(
+              build: (format) => generatePdf(format, snapshot.data!),
+              // show snackbar on share
+              maxPageWidth: 600,
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
